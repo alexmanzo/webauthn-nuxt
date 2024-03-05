ece82b15435b07255fd6bfe13585d99b88e7c3ee
Alex Manzo <amanzo@fundrise.com>
2024-03-05 12:09:52 -0500

    Set up basic form.

---
+++ pages/register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card flex justify-content-center">
+    <InputText type="email" v-model="email" />
+    <Button label="Submit" @click="createUser" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const email: Ref<string | null> = ref(null);
+
+async function createUser() {
+  const { data, pending, error, refresh } = await useFetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({ email: email.value }),
+  });
+  console.log({data, pending, error, refresh});
+}
+</script>
--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -11,10 +11,7 @@ export default defineEventHandler(async (event) => {
   const payload = await readValidatedBody(event, (body) => userSchema.safeParse(body)); // or `.parse` to directly throw an error
 
   if (!payload.success) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: result.error.issues,
-    });
+    throw payload.error.issues;
   }
 
   try {
@@ -22,12 +19,9 @@ export default defineEventHandler(async (event) => {
       data: payload.data,
     });
   } catch (e) {
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      // The .code property can be accessed in a type-safe manner
-      if (e.code === 'P2002') {
-        console.log('There is a unique constraint violation, a new user cannot be created with this email');
-      }
-    }
-    throw e;
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'An error occurred.'
+    });
   }
 });
